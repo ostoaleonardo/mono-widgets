5dfbf69910990fa709684fcca9613349ba4c89ef
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-03-30 17:50:01 -0600

    Add expo-font

--- app/index.js
+++ app/index.js
@@ -1,9 +1,32 @@
+import { useCallback } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import * as SplashScreen from 'expo-splash-screen'
+import { useFonts } from 'expo-font'
+import { COLORS, FONTS } from '@/constants'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+    const [fontsLoaded, fontError] = useFonts({
+        'Roboto-Mono': require('../assets/fonts/RobotoMono.ttf'),
+    })
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded || fontError) {
+            await SplashScreen.hideAsync()
+        }
+    }, [fontsLoaded, fontError])
+
+    if (!fontsLoaded && !fontError) {
+        return null
+    }
+
     return (
-        <View style={styles.container}>
+        <View style={styles.container} onLayout={onLayoutRootView}>
+            <Text style={styles.title}>
+                Mono Widgets
+            </Text>
             <StatusBar style='auto' />
         </View>
     )
@@ -12,5 +35,12 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
+    title: {
+        fontSize: 16,
+        color: COLORS.text,
+        fontFamily: FONTS.mono,
+    }
 })
